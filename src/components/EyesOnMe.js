273dc9ee73cd823f9be88ba7ea33483ccd2ebaec
Kiana Long <kianajade42.kl@gmail.com>
2021-05-12 22:38:19 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -4,7 +4,7 @@ class EyesOnMe extends React.Component
 {
   onFocus=()=> console.log=('Good!')
   onBlur()=> console.log=('Hey! Eyes on me!')
-  
+
   render(){
     return(
       <button>
@@ -12,8 +12,8 @@ class EyesOnMe extends React.Component
       onBlur={this.onBlur}
       Eyes on me, please!
       </button>
-      
-      
+
+
     )
   }
 }
